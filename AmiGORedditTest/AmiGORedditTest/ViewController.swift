801511c7089dcb2d45f691a7f5e8462fa8aebdc1
Rafael Bonini <rafael.souza@picpay.com>
2021-10-09 22:08:23 -0300

    Cleaning up base files

--- AmiGORedditTest/AmiGORedditTest/ViewController.swift
+++ AmiGORedditTest/AmiGORedditTest/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  AmiGORedditTest
-//
-//  Created by Rafael Souza on 09/10/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
