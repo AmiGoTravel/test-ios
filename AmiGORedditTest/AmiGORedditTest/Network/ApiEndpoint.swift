598ea9c4fa70f95faa82cfb15052cddd0a37dd71
Rafael Bonini <rafael.souza@picpay.com>
2021-10-10 23:54:19 -0300

    Updating `ApiEndpoint` to use URLComponents to build the URL

--- AmiGORedditTest/AmiGORedditTest/Network/ApiEndpoint.swift
+++ AmiGORedditTest/AmiGORedditTest/Network/ApiEndpoint.swift
@@ -1,17 +1,22 @@
 import Foundation
 
 protocol ApiEndpoint {
-    var baseURL: URL { get }
+    var baseHost: String { get }
     var path: String { get }
+    var queryItems: [URLQueryItem] { get }
 }
 
 extension ApiEndpoint {
-    var baseURL: URL { URL(string: "https://api.reddit.com")! }
+    var baseHost: String { "api.reddit.com" }
     
-    var absoluteStringUrl: String {
-        let basePathString = baseURL.absoluteString
-        let safePath = path.starts(with: "/") || path.isEmpty ? path : "/\(path)"
-        return "\(basePathString)\(safePath)"
+    var urlComponents: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = baseHost
+        components.path = path
+        components.queryItems = queryItems
+        
+        return components
     }
 }
 
