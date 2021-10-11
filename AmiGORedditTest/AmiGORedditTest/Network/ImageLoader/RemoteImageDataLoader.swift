20a4ba176ad7a5ae6cd2abe728a6dfbd02836071
Rafael Bonini <rafael.souza@picpay.com>
2021-10-11 16:09:57 -0300

    Adding image loader to handle image request with task cancelation and emptying completion

---
+++ AmiGORedditTest/AmiGORedditTest/Network/ImageLoader/RemoteImageDataLoader.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+final class RemoteImageDataLoader: ImageDataLoader {
+    private let client: HTTPClient
+    
+     init(client: HTTPClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))) {
+        self.client = client
+    }
+    
+     enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    private final class HTTPClientTaskWrapper: ImageDataLoaderTask {
+        private var completion: ((ImageDataLoader.Result) -> Void)?
+        
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (ImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: ImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+     func loadImageData(from url: URL, completion: @escaping (ImageDataLoader.Result) -> Void) -> ImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap { (data, response) in
+                    let isValidResponse = response.isOK && !data.isEmpty
+                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                })
+        }
+        return task
+    }
+}
