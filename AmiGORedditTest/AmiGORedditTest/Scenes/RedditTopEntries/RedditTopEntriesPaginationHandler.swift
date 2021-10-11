1d74e97db5e23ec0d01fe7da8b41de965a5e476d
Rafael Bonini <rafael.souza@picpay.com>
2021-10-11 15:36:36 -0300

    Mapping models into cell controllers

--- AmiGORedditTest/AmiGORedditTest/Scenes/RedditTopEntries/RedditTopEntriesPaginationHandler.swift
+++ AmiGORedditTest/AmiGORedditTest/Scenes/RedditTopEntries/RedditTopEntriesPaginationHandler.swift
@@ -1,10 +1,23 @@
 import Foundation
 
 final class RedditTopEntriesPaginationHandler {
-    private(set) var entriesList = [RedditChildrenData]()
+    private(set) var cellControllers = [RedditTopEntryCellController]()
     private(set) var after = ""
+    var shouldResetPagination = false
+    private var entriesList = [RedditChildrenData]() {
+        didSet {
+            cellControllers.append(contentsOf: entriesList.map { mapToController($0) })
+        }
+    }
     var entriesCount: Int {
         entriesList.count
+    }
+    
+    private func mapToController(_ model: RedditChildrenData) -> RedditTopEntryCellController {
+        let viewModel = RedditTopEntryCellViewModel(model: model)
+        let controller = RedditTopEntryCellController(viewModel: viewModel)
+        viewModel.controllerDelegate = controller
+        return controller
     }
     
     func updatePagination(with rootModel: RedditAPIRootModel) {
