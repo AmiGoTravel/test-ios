e342dcd3016bceba2a4a3345f196331763868425
Rafael Bonini <rafael.souza@picpay.com>
2021-10-11 18:58:38 -0300

    Loading image to display in details scene

--- AmiGORedditTest/AmiGORedditTest/Scenes/RedditEntryDetails/RedditEntryDetailsViewController.swift
+++ AmiGORedditTest/AmiGORedditTest/Scenes/RedditEntryDetails/RedditEntryDetailsViewController.swift
@@ -1,15 +1,25 @@
 import UIKit
 
 protocol RedditEntryDetailsViewProtocol: AnyObject {
-    
+    func showThumbnailImage(data: Data)
 }
 
 final class RedditEntryDetailsViewController: UIViewController, Storyboarded {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     
+    var viewModel: RedditEntryDetailsDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel?.requestImage()
+        titleLabel.text = viewModel?.title
+    }
 }
 
 extension RedditEntryDetailsViewController: RedditEntryDetailsViewProtocol {
-    
+    func showThumbnailImage(data: Data) {
+        let image = UIImage(data: data)
+        thumbnailImageView.image = image
+    }
 }
--- AmiGORedditTest/AmiGORedditTest/Scenes/RedditEntryDetails/RedditEntryDetailsViewModel.swift
+++ AmiGORedditTest/AmiGORedditTest/Scenes/RedditEntryDetails/RedditEntryDetailsViewModel.swift
@@ -1,11 +1,36 @@
 import Foundation
 
+protocol RedditEntryDetailsDelegate {
+    func requestImage()
+    var title: String { get }
+}
+
 final class RedditEntryDetails {
     weak var controllerDelegate: RedditEntryDetailsViewProtocol?
     weak var coordinator: MainCoordinator?
     private let entryModel: RedditChildrenData
+    private let imageLoader: ImageDataLoader
     
-    init(entryModel: RedditChildrenData) {
+    init(entryModel: RedditChildrenData,
+         imageLoader: ImageDataLoader = MainThreadDispatchDecorator(RemoteImageDataLoader())) {
         self.entryModel = entryModel
+        self.imageLoader = imageLoader
+    }
+}
+
+extension RedditEntryDetails: RedditEntryDetailsDelegate {
+    var title: String { entryModel.title }
+    
+    func requestImage() {
+        guard let url = URL(string: entryModel.thumbnail) else { return }
+        
+        imageLoader.loadImageData(from: url) { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.controllerDelegate?.showThumbnailImage(data: data)
+            case .failure:
+                break
+            }
+        }
     }
 }
