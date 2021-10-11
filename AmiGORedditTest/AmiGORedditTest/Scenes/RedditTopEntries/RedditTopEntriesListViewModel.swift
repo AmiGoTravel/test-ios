8b0f6f3edd6f98eb41ccc1947d871cfbda3532b7
Rafael Bonini <rafael.souza@picpay.com>
2021-10-11 15:44:57 -0300

    Get service completion in the main thread, expose number of entries and cell controllers to the viewController

--- AmiGORedditTest/AmiGORedditTest/Scenes/RedditTopEntries/RedditTopEntriesListViewModel.swift
+++ AmiGORedditTest/AmiGORedditTest/Scenes/RedditTopEntries/RedditTopEntriesListViewModel.swift
@@ -3,6 +3,7 @@ import Foundation
 protocol RedditTopEntriesListViewModelProtocol {
     var numberOfEntries: Int { get }
     func fetchEntries()
+    func cellController(forRowAt indexPath: IndexPath) -> RedditTopEntryCellController
 }
 
 final class RedditTopEntriesListViewModel {
@@ -10,22 +11,27 @@ final class RedditTopEntriesListViewModel {
     private let service: RedditTopEntriesListServiceProtocol
     private let paginationHandler = RedditTopEntriesPaginationHandler()
     
-    var numberOfEntries: Int { paginationHandler.entriesCount }
-    
     init(controllerDelegate: RedditTopEntriesListViewControllerProtocol,
-         service: RedditTopEntriesListServiceProtocol = RedditTopEntriesListService()) {
+         service: RedditTopEntriesListServiceProtocol = MainThreadDispatchDecorator(RedditTopEntriesListService())) {
         self.service = service
+        self.controllerDelegate = controllerDelegate
     }
 }
 
 extension RedditTopEntriesListViewModel: RedditTopEntriesListViewModelProtocol {
+    var numberOfEntries: Int { paginationHandler.entriesCount }
+    
+    func cellController(forRowAt indexPath: IndexPath) -> RedditTopEntryCellController {
+        return paginationHandler.cellControllers[indexPath.row]
+    }
+    
     func fetchEntries() {
         service.fetchTopEntries(paginationHandler) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case let .success(redditModel):
                 self.paginationHandler.updatePagination(with: redditModel)
-                self.controllerDelegate?.displayEntries(from: self.paginationHandler.entriesList)
+                self.controllerDelegate?.displayEntries()
             case .failure:
                 break
             }
